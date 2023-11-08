dbe68d6edeece18baed742bdb2e0e2b603a7be92
Viplov Patney <viplovpatney@192.168.1.139>
2023-11-08 19:55:39 +0000

    Changes based on adsense warnign

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 document.getElementById('calculate-revenue-button').addEventListener('click', calculateRevenue);
 
 function calculateRevenue() {
-  const adType = document.getElementById('adType').value;
   const impressions = parseInt(document.getElementById('impressions').value);
   const clicks = parseInt(document.getElementById('clicks').value);
 
@@ -10,23 +9,10 @@ function calculateRevenue() {
     return;
   }
 
-  let totalRevenue = 0;
+  const cpm = 5; // Example CPM for demonstration
+  const cpc = 0.5; // Example CPC for demonstration
 
-  if (adType === 'adsense') {
-    const cpm = 5; // Example CPM for demonstration
-    const cpc = 0.5; // Example CPC for demonstration
-
-    totalRevenue = (impressions * (cpm / 1000)) + (clicks * cpc);
-  } else if (adType === 'amazon') {
-    // Perform Amazon Affiliate calculations based on provided fields
-    // Update logic based on how the Amazon Affiliate revenue is calculated
-    // Example calculation:
-    const conversionRate = (clicks / impressions) * 100; // Conversion rate in percentage
-    const averageSaleAmount = 50;
-    const commissionRate = 0.1;
-
-    totalRevenue = (conversionRate * averageSaleAmount * commissionRate) / 100;
-  }
+  const totalRevenue = (impressions * (cpm / 1000)) + (clicks * cpc);
 
   document.getElementById('result').innerHTML = `Estimated Revenue: $${totalRevenue.toFixed(2)}`;
 }
